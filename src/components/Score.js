72f8ef392a0f3298faed1ae12d02c8c67f75ab29
Unwana Essien <unwana.essien@outlook.com>
2018-10-30 10:04:11 +0100

    Updated  the score mechanism

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const C = styled.div`
+    padding: 20px;
+    border-radius: 15px;
+    border: 2px solid #99E1D9;
+    width: 360px;
+    margin: 20px auto;
+    font-size: 22px;
+`
 
 export default class Scores extends Component {
     render() {
+        const {roles, score} = this.props
         return (
-            <div>
-                This is where the score will be.
-            </div>
+            <C>
+                Human({roles.human}): {score.human}<br />
+                Computer({roles.computer}): {score.computer}
+            </C>
         )
     }
 }
